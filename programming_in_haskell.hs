f66441b5a4c03912aede6a42c8b2a86667c3a32b
David Draguta <daviddraguta@gmail.com>
2020-11-29 14:09:29 -0500

    QWE

---
+++ programming_in_haskell.hs
@@ -0,0 +1,12 @@
+qsort :: Ord a => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
+  where
+    smaller = [a | a <- xs, a <= x]
+    larger  = [b | b <- xs, b > x]
+
+seqn :: Monad m => [m a] -> m [a]
+seqn (act:acts) = do x <- act
+                     xs <- seqn acts
+                     return (x:xs)
+

8c5568f7abb4e41168d4b5e35c1cb9a6d8895d32
David Draguta <daviddraguta@gmail.com>
2020-12-01 21:04:53 -0500

    qwe

--- programming_in_haskell.hs
+++ programming_in_haskell.hs
@@ -1,6 +1,9 @@
 import Data.Char
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------
+divides :: Int -> Int -> Bool
+divides m n = mod n m == 0 
+---------------------------------------------------------------------------------------------------------------------------------------------------------
   
 qsort :: Ord a => [a] -> [a]
 qsort [] = []
@@ -115,13 +118,11 @@ luhnDouble :: Int -> Int
 luhnDouble x | a > 9     = a-9
              | otherwise = a
              where a = x * 2
-luhn :: Int -> Int -> Int -> Int -> Bool
-luhn a b c d = mod (sum [luhnDouble a, b, luhnDouble c, d]) 10 == 0
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------
 
 factors :: Int -> [Int]
-factors n = [x | x <- [1..n], n `mod` x == 0]
+factors n = [x | x <- [1..n], x `divides` n]
 
 prime :: Int -> Bool
 prime n = factors n == [1,n]
@@ -353,20 +354,20 @@ make8 bits = take 8 (bits ++ myRepeat 0)
 
 --Encodes string by mapping its chars to a unicode number, then converting each unicode number to an 8bit binary number
 encodeString :: String -> [Bit]
-encodeString = concat . map (make8 . int2bin . ord)
+encodeString = concatMap (addParityBit . make8 . int2bin . ord)
 
 --To decode the strict, we must chop it back into 8 bit binary numbers. To that end we define:
-chop8 :: [Bit] -> [[Bit]]
-chop8 []   = []
-chop8 bits = take 8 bits : chop8 (drop 8 bits)
+--chop :: Int -> [Bit] -> [[Bit]]
+--chop n []   = []
+--chop n bits = take n bits : chop n (drop 8 bits)
 
 --And now we just undo what we did in encodeString:
 decodeBits :: [Bit]->String 
-decodeBits = map (chr . bin2int) . chop8
+decodeBits = map (chr . bin2int . processBits) . chop 9
 
 --We model a perfect communication channel using the identity function, and simulate the transmission of a string as a list of bits: 
 transmit :: String -> String
-transmit = decodeBits . channel . encodeString
+transmit = decodeBits . badChannel . encodeString 
 
 channel :: [Bit] -> [Bit]
 channel = id 
@@ -401,19 +402,81 @@ myDropWhile p (x:xs) | p x       = myDropWhile p xs
 
   --3. Redefine the functions map f and filter p using foldr
   
-  --The following is just to get an intuition.
-  --foldr f v []            = z
-  --foldr f v (x:xs)        = x `f` foldr f v xs
-  --foldr f v [a_1,...,a_n] = a_1 `f` (a_2 `f` (...(a_n `f` v)...))
+  --   The following is just to get an intuition.
+  --   foldr f v []            = z
+  --   foldr f v (x:xs)        = x `f` foldr f v xs
+  --   foldr f v [a_1,...,a_n] = a_1 `f` (a_2 `f` (...(a_n `f` v)...))
   
-myMap :: (a -> b) -> [a] -> [b]
-myMap f = foldr (\x y -> (f x): y) [] -- then myMap f [a_1, ..., a_n] = f a_1 : foldr (\x y -> (f x): y) [] [a_2, ..., a_n] = ...
+--myMap :: (a -> b) -> [a] -> [b]
+--myMap f = foldr (\x y -> (f x): y) [] -- then myMap f [a_1, ..., a_n] = f a_1 : foldr (\x y -> (f x): y) [] [a_2, ..., a_n] = ...
                                       --                              = f a_1 : ... : f a_n : [] = [f a_1, ..., f a_n]
 myFilter :: (a -> Bool) -> [a] -> [a]
 myFilter p = foldr (\x y -> if p x then x:y else y) []
 
   --4. Using foldl, define a function dec2int :: [Int] -> Int that converts a decimal number into an integer. For example:
-  --   dec2int [2,3,4,5] = 2345
-  --foldl f v []            = v
-  --foldl f v (x:xs)        = foldl f (v `f` x) xs
-  --foldl f v [a_1,...,a_n] = (...((v `f` a_1) `f` a_2)...) `f` a_n
+  --   dec2int [2,3,4,5] = 2345 = 2*10^3 + 3*10^2 + 4*10^1 + 5*10^0 = 5 + 10(4 + 10(3 + 10(2 + 10*0))) = (((0*10+2)*10 + 3)*10 + 4)*10 + 5,
+  --   which with the following gives us our answer:
+  --   foldl f v []            = v
+  --   foldl f v (x:xs)        = foldl f (v `f` x) xs
+  --   foldl f v [a_1,...,a_n] = (...((v `f` a_1) `f` a_2)...) `f` a_n
+dec2int :: [Int] -> Int
+dec2int = foldl (\x y -> 10*x+y) 0
+
+  --5. Without looking at the definitions from the standard prelude, define the higher-order library function ucrry that converts a function on
+  --   pairs into a curried function, and, conversely, the function uncurry that converts a curried function with two arguments into a function 
+  --   on pairs.
+  
+myCurry :: ((a, b) -> c) -> (a -> b -> c)
+myCurry f = \x -> \y -> f (x, y)
+myUncurry :: (a -> b -> c) -> ((a,b) -> c)
+myUncurry f = \(x, y) -> f x y
+
+  --6. A higher-order function unfold that encapsulates a simple pattern of recursion for producing a list can be defined as follows:
+unfold p h t x | p x       = []
+               | otherwise = h x : unfold p h t (t x)
+  --   That is, the function unfold p h t produces the empty list if the predicate p is true of the argument value, and otherwise produces
+  --   a non-empty list by applying the function h to this value to give the head, and the function t to generate another argument that is
+  --   recursively processed in the same way to produce the tail of the list. For example, the function int2bin can be rewritten more compactly
+  --   using unfold as follows
+  --   int2bin = unfold (== 0) (`mod` 2) (`div` 2)
+  --   Redefine the functions chop8, map f and iterate f using unfold.
+  
+chop :: Int -> [Bit] -> [[Bit]]
+chop n = unfold null (take n) (drop n)
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap f = unfold null (f . head) tail
+
+--iterate f x = [x, f x, f (f x), f (f (f x)), ...]
+myIterate :: (a -> a) -> a -> [a] 
+myIterate = unfold (const False) id
+
+  --7. Modify the binary string transmitter example to detect simple tranmission errors using the concept of parity bits. That is, each eight-bit binary number produced during encoding is 
+  --   extended with a parity bit, set to one if the number contains an odd number of ones, and to zero otherwise. In turn, each resulting nine-bit binary number consumed during decoding
+  --   is checked to ensure that its parity bit is correct, with the parity bit being discarded if this is the case, and a parity error being reported otherwise.
+
+parityBit :: [Bit] -> Bit
+parityBit bs = mod (sum $ filter (== 1) bs) 2
+
+addParityBit :: [Bit] -> [Bit]
+addParityBit bs = parityBit bs : bs 
+
+checkParityBit :: [Bit] -> Bool
+checkParityBit bs = parityBit (tail bs) == head bs 
+
+processBits :: [Bit] -> [Bit]
+processBits bs | checkParityBit bs = tail bs 
+               | otherwise         = error "Transmission error!"
+  --8. Test your new string transmitter program from the previous exercise using a faulty communication channel that forgets the first bit, which can be modelled using the tail 
+  --   function on lists of bits.
+badChannel :: [Bit] -> [Bit]
+badChannel = tail
+
+  --9. Define a function altMap :: (a -> b) -> (a -> b) -> [a] -> [b] that alternately applies its two argument functions to successive elements in a list, in turn about order.
+  --   eg: altMap (+10) (+100) [0,1,2,3,4] = [10,101,12,103,14]
+  
+altMap :: (a -> b) -> (a -> b) -> [a] -> [b]
+altMap _ _ []     = []
+altMap f g (x:xs) = f x : altMap g f xs
+
+luhn xs = 10 `divides` sum (altMap luhnDouble id xs)
